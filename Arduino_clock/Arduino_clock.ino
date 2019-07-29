f365f890d49c101a933b9b6c2aac9922b26d8168
xianfei <979719361@qq.com>
2019-07-29 22:56:33 +0800

    v4
    1.添加对于外置高精度时钟模块（DS3231）的支持，可以在淘宝购买，并在代码中添加#define RTC_DS3231即可，这样断电之后时间仍然会接着走，而且误差很小。
    2.在编译之后会自动使用编译时的时间初始化时钟的时间，需要根据自己电脑编译速度等因素调节宏TIMEOFFSET的值，默认为10。
    3.手动设置时间时不再需要输入星期，用蔡勒公式自动计算年月日所对应的星期。

--- Arduino_clock/Arduino_clock.ino
+++ Arduino_clock/Arduino_clock.ino
@@ -1,97 +1,105 @@
 
-/*功能说明：
-   电源键：返回主界面
-   1键：修改时间
-   2键：设定闹钟
-   3键：修改日期
-   4键：取消闹钟
-   5键：切换24小时制
-   6键：显示温度
-   7键：切换背光开关
-修改时间日期及设置闹钟使用0-9键输入
-显示alamr则表示闹钟已设定
-
-v3.1 
-加入亮度缓升缓降
-加入防PWM调光干扰（亮度均值采样）
-加入低亮度按遥控器保持五秒背光
-修正输入月份可能进位的bug
-*/
-
-//在这里自定义遥控按键
-#define edittimeKey 1
-#define setAlarmKey 2
-#define editdayKey 3
-#define cancelAlarmKey 4
-#define use24hKey 5
-#define showtempKey 6
-#define backlightKey 7
+/********** 需要用到的库（在Arduino库管理器中安装）**********
+    IRremote      LiquidCrystal I2C     Rtc by Makuna
+ *******************************************************/
+//在这里自定义数字区遥控按键
+#define edittimeKey 1 // 1键：修改时间
+#define setAlarmKey 2 // 2键：设定闹钟
+#define editdayKey 3 // 3键：修改日期
+#define cancelAlarmKey 4 // 4键：取消闹钟
+#define use24hKey 5 // 5键：切换24小时制
+#define showtempKey 6 // 6键：显示温度
+#define backlightKey 7 // 7键：打开或关闭背光
 
 //在这里设置端口信息
-#define DHT11_PIN 5  //温度计模块接口
+#define DHT11_PIN 5  //温度计模块接口 删除这句话会禁用温度相关代码
 #define buzzpin 4  //蜂鸣器接收端口
-#define LCD_BK_PIN 6  //温度计模块接口
 #define RECV_PIN 10 //红外接收端口
+
 #define AMBIENT_Light1 9
 #define AMBIENT_Light2 3
-#define LIGHT_SENSOR A0
 
-//在这里设定初始化的 时 分 秒 年 月 日 星期
-static short h = 18, m = 16, s = 30, y = 2019, mon = 7, day = 21, xq = 7;
+#define LCD_BK_PIN 6  //屏幕背光接口（如果使用自动亮度调节） 删除这句话会禁用自动亮度相关代码
+#define LIGHT_SENSOR A0 //光线传感器接口（如果使用自动亮度调节） 删除这句话会禁用自动亮度相关代码
 
-//在这里自定义是否使用24h制、显示温度
-bool use24h=1,showtemp=0;
+#define TIMEOFFSET 10 // 时钟时间与编译时间补偿  具体数值取决于编译及上传速度等
 
-//在这里自定义启动画面显示的信息
-#define bootinf "by xianfei"
+#define bootinf "by xianfei"  //自定义启动画面显示的信息 如不想显示任何信息请直接删除或注释掉该行
 
+// #define RTC_DS3231  //在这里定义是否要启动外置时间模块  注释掉这句话则不使用外置时钟模块
+//外置时间模块需要自己单独购买，淘宝搜DS3231我买的六块八包邮
+
+/*******************************************************/
 
 //以下是代码部分
-#include "dht11.h"
-dht11 DHT;
+#if defined(RTC_DS3231)
+  #include <Wire.h> // must be included here so that Arduino library object file references work
+  #include <RtcDS3231.h>
+  RtcDS3231<TwoWire> Rtc(Wire);
+#endif
+#include <inttypes.h>
+#include <Arduino.h>
+#if defined(DHT11_PIN)
+  #include "dht11.h"
+  dht11 DHT;
+#endif
 #include <LiquidCrystal_I2C.h>
 #include <IRremote.h>
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 LiquidCrystal_I2C lcd(0x20, 16, 2); //设置LCD的地址，2行，每行16字符
 static char t[9], d[12];
+static short h, m, s, y, mon, day, xq; // 系统时间 时 分 秒 年 月 日 星期
+static short h_, m_, s_, y_, mon_, day_, xq_; // 调整时间的临时变量 时 分 秒 年 月 日 星期
 static short edittime = 0, editdate = 0, editalarm = 0, curs,backLight=1;
 static short ah = 25, am = 0;
+unsigned long oldmillis=0;
+bool use24h=1,showtemp=0; //是否使用24h制、显示温度的初始状态
 #define editmode (edittime || editdate || editalarm)
-
 
 unsigned char ambientLight1 = 0;
 bool ambientLight1Waved = false;
 unsigned char ambientLight2 = 0;
 bool ambientLight2On = false;
-unsigned long oldmillis=0;
-unsigned long oldmillis2=0;
+
 bool noOutputOnce = false;
-unsigned char currentBright=255;  //  当前显示屏亮度  用于亮度缓慢升降
-unsigned char lcd_bk = 255;  //  应设定的显示屏亮度
-unsigned char bright[100]={0};  // 用于亮度采样  防止PWM影响亮度检测
-unsigned char bright_i=0;  // 用于亮度采样的循环变量
+#if defined(LIGHT_SENSOR)&&(LCD_BK_PIN)
+  unsigned long oldmillis2=0;
+  unsigned char currentBright=255;  //  当前显示屏亮度  用于亮度缓慢升降
+  unsigned char lcd_bk = 255;  //  应设定的显示屏亮度
+  unsigned char bright[100]={0};  // 用于亮度采样  防止PWM影响亮度检测
+  unsigned char bright_i=0;  // 用于亮度采样的循环变量
+#endif
 unsigned char keepBKOn=0; //  保持背光时间（s）
 
 void setup()
 {
   lcd.init();         // LCD初始化设置
   lcd.backlight();    // 打开 LCD背光
-  pinMode(LCD_BK_PIN, OUTPUT);
+  #if defined(LIGHT_SENSOR)&&(LCD_BK_PIN)
+    pinMode(LCD_BK_PIN, OUTPUT);
+    analogWrite(LCD_BK_PIN, lcd_bk);
+  #endif
   pinMode(AMBIENT_Light1, OUTPUT);
-
   //analogWrite(AMBIENT_Light1, 255);
-  analogWrite(LCD_BK_PIN, lcd_bk);
   Serial.begin(9600); // 设置串口波特率9600
   irrecv.enableIRIn();
   pinMode(buzzpin, OUTPUT);
-  lcd.setCursor(0, 0);
-  lcd.print("Booting...");
-  lcd.setCursor(0, 1);
-  lcd.print(bootinf);
-  delay(1000);
+  #if defined(bootinf)
+    lcd.setCursor(0, 0);
+    lcd.print("Booting...");
+    lcd.setCursor(0, 1);
+    lcd.print(bootinf);
+    delay(1000);
+  #endif
   lcd.clear();
- 
+  #if defined(RTC_DS3231)
+    Rtc.Begin();
+    RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__)+TIMEOFFSET;
+    if (Rtc.GetDateTime() < compiled) Rtc.SetDateTime(compiled);
+  #else
+    inittime();
+  #endif
 }
 
 
@@ -105,9 +113,9 @@ void loop()
     //double light=255*sqrt(0.5*(1.0+sin(millis()/2000.0)));
     double light=ambientLight1*pow(0.5*(1.0+sin(millis()/2000.0)),2);
     analogWrite(AMBIENT_Light1, light);
-    Serial.println(light);
+    //Serial.println(light);
     }
-  #ifndef TEST1
+  #if defined(LIGHT_SENSOR)&&(LCD_BK_PIN)
   int sensorValue = analogRead(LIGHT_SENSOR); // 0-1023
   bright[bright_i++]=sensorValue/4;  // 0-255
   if(bright_i==100){
@@ -120,13 +128,13 @@ void loop()
     for(int i=0;i<100;i++)sum+=bright[i];
     if(keepBKOn!=0&&currentBright<=50)currentBright=50;// 黑暗下 遥控等操作出发背光短暂亮起
     else {lcd_bk=sum/100;  // 
-    Serial.println("lcd_bk");
-    Serial.println(lcd_bk);
+    //Serial.println("lcd_bk");
+    //Serial.println(lcd_bk);
     if(lcd_bk<currentBright&&currentBright>0)currentBright--;
     if(lcd_bk>currentBright&&currentBright<255)currentBright++;}
     //if(keepBKOn!=0&&currentBright<100)analogWrite(LCD_BK_PIN, 100);
-    Serial.println("currentBright");
-    Serial.println(currentBright);
+    //Serial.println("currentBright");
+    //Serial.println(currentBright);
     analogWrite(LCD_BK_PIN, currentBright);
     
    }
@@ -134,7 +142,7 @@ void loop()
   unsigned long millis_=millis();
   if(millis_-oldmillis>=1000){
     oldmillis=millis_;
-    //Serial.println(millis());
+    Serial.println(millis());
     if(keepBKOn>0)keepBKOn--;
     if (!edittime) gettime(); //获取时间及时间流动
     if (!(editmode || (ah == h && am == m))) {
@@ -151,11 +159,19 @@ void recv()
     {
       if(results.value>0xFD0000&&results.value<0xFDFFFF)keepBKOn=5;
       Serial.println(results.value, HEX);
-      if (results.value == 0xFD807F) // VOL+
+      if (results.value == 0xFD00FF)  // 电源键：返回主界面
+      {
+        edittime = 0;
+        editdate = 0;
+        editalarm = 0;
+        lcd.clear();
+        lcd.noBlink();
+        ah = 25, am = 0;
+        digitalWrite(buzzpin, LOW);
+      }else if (results.value == 0xFD807F) // VOL+ 键
       {
         // do sth...
-      }
-      if (results.value == 0xFD40BF) // FUNC/STOP
+      }else if (results.value == 0xFD40BF) // FUNC/STOP  切换非PWM灯光控制
       {
         if(ambientLight2==0){
           ambientLight2=1;
@@ -166,7 +182,7 @@ void recv()
         lcd.setCursor(6,1);
         lcd.print("OFF");
           pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+          pinMode(4, OUTPUT);
           digitalWrite(3, LOW);
           digitalWrite(4, LOW);
          }else if(ambientLight2==1){
@@ -178,7 +194,7 @@ void recv()
         lcd.setCursor(6,1);
         lcd.print("LOW");
           pinMode(3, OUTPUT);
-  pinMode(4, INPUT);
+          pinMode(4, INPUT);
           digitalWrite(3, HIGH);
           //digitalWrite(4, LOW);
           }else if(ambientLight2==2){
@@ -190,24 +206,20 @@ void recv()
         lcd.setCursor(6,1);
         lcd.print("HIGH");  
           pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+          pinMode(4, OUTPUT);
           digitalWrite(3, HIGH);
           digitalWrite(4, HIGH);
           }
-      }
-      if (results.value == 0xFD20DF) // |<<
+      }else if (results.value == 0xFD20DF) // |<<
       {
         // do sth...
-      }
-      if (results.value == 0xFDA05F) // >||
+      }else if (results.value == 0xFDA05F) // >||
       {
         // do sth...
-      }
-      if (results.value == 0xFD609F) // >>|
+      }else if (results.value == 0xFD609F) // >>|
       {
         // do sth...
-      }
-      if (results.value == 0xFD10EF) // 下箭头
+      }else if (results.value == 0xFD10EF) // 下箭头  切换PWM控制灯光亮度
       {
         if(ambientLight1>=10) ambientLight1 -= 50;
         if(ambientLight1==5) ambientLight1 = 0;
@@ -219,8 +231,7 @@ void recv()
         lcd.setCursor(6,1);
         lcd.print(ambientLight1/50);
         lcd.print(" / 5");
-      }
-      if (results.value == 0xFD906F) // VOL-
+      }else if (results.value == 0xFD906F) // VOL-   切换PWM控制灯光呼吸效果
       {
         if(!ambientLight1Waved){
           ambientLight1Waved=true;
@@ -239,8 +250,7 @@ void recv()
         lcd.setCursor(4,1);
         lcd.print("WavedOFF");
             }
-      }
-      if (results.value == 0xFD50AF) // 上箭头
+      }else if (results.value == 0xFD50AF) // 上箭头   切换PWM控制灯光亮度
       {
         if(ambientLight1<=245) ambientLight1 += 50;
         if(ambientLight1==250) ambientLight1 = 255;
@@ -253,64 +263,43 @@ void recv()
         lcd.setCursor(6,1);
         lcd.print(ambientLight1/50);
         lcd.print(" / 5");
-      }
-      if (results.value == 0xFDB04F) // EQ
+      }else if (results.value == 0xFDB04F) // EQ
       {
         // do sth...
-      }
-      if (results.value == 0xFD708F) // ST/REPT
+      }else if (results.value == 0xFD708F) // ST/REPT
       {
         // do sth...
-      }
-      if (results.value == 0xFD30CF)
+      }else if (results.value == 0xFD30CF)
+      /*****数字键区功能请不要从这里修改*****/
       {
         remote(0);
-      }
-      if (results.value == 0xFD08F7)
+      }else if (results.value == 0xFD08F7)
       {
         remote(1);
-      }
-      if (results.value == 0xFD8877)
+      }else if (results.value == 0xFD8877)
       {
         remote(2);
-      }
-      if (results.value == 0xFD48B7)
+      }else if (results.value == 0xFD48B7)
       {
         remote(3);
-      }
-      if (results.value == 0xFD28D7)
+      }else if (results.value == 0xFD28D7)
       {
         remote(4);
-      }
-      if (results.value == 0xFDA857)
+      }else if (results.value == 0xFDA857)
       {
         remote(5);
-      }
-      if (results.value == 0xFD6897)
+      }else if (results.value == 0xFD6897)
       {
         remote(6);
-      }
-      if (results.value == 0xFD18E7)
+      }else if (results.value == 0xFD18E7)
       {
         remote(7);
-      }
-      if (results.value == 0xFD9867)
+      }else if (results.value == 0xFD9867)
       {
         remote(8);
-      }
-      if (results.value == 0xFD58A7)
+      }else if (results.value == 0xFD58A7)
       {
         remote(9);
-      }
-      if (results.value == 0xFD00FF)
-      {
-        edittime = 0;
-        editdate = 0;
-        editalarm = 0;
-        lcd.clear();
-        lcd.noBlink();
-        ah = 25, am = 0;
-        digitalWrite(buzzpin, LOW);
       }
       irrecv.resume();
     }
@@ -378,12 +367,14 @@ void output()
   default:
     lcd.print("Err");
   }
+  #if defined(DHT11_PIN)
   if(showtemp&&(s%2==0)){
     DHT.read(DHT11_PIN);
     lcd.print(" ");
     if(0<DHT.temperature&&DHT.temperature<50)
     lcd.print(DHT.temperature,1);
   }
+  #endif
 }
 
 void alarmrun()
@@ -409,18 +400,17 @@ int remote(int recv)
 {
   if (recv == backlightKey && !editmode){
     backLight=!backLight;
-    if(backLight){lcd.backlight(); 
-    analogWrite(LCD_BK_PIN, lcd_bk);
-    }
-    else {lcd.noBacklight();
-    analogWrite(LCD_BK_PIN, 0);}
+    if(backLight)lcd.backlight(); 
+    else lcd.noBacklight();
     return 0;
   }
+  #if defined(DHT11_PIN)
   if (recv == showtempKey && !editmode){
     showtemp=!showtemp;
     lcd.clear();
     return 0;
   }
+  #endif
   if (recv == use24hKey && !editmode){
     use24h=!use24h;
     lcd.clear();
@@ -529,26 +519,32 @@ int remote(int recv)
     switch (curs)
     {
     case 3:
-      h = recv * 10;
+      h_ = recv * 10;
       break;
     case 4:
-      h = h + recv;
+      h_ = h_ + recv;
       break;
     case 6:
-      m = recv * 10;
+      m_ = recv * 10;
       break;
     case 7:
-      m = m + recv;
+      m_ = m_ + recv;
       break;
     case 9:
-      s = recv * 10;
+      s_ = recv * 10;
       break;
     case 10:
-      s = s + recv;
+      s_ = s_ + recv;
       break;
     }
     if (curs == 10)
     {
+      h=h_;
+      s=s_;
+      m=m_;
+      #if defined(RTC_DS3231)
+      Rtc.SetDateTime(RtcDateTime{y,mon,day,h_,m_,s_});
+      #endif
       lcd.clear();
       edittime = 0;
       lcd.noBlink();
@@ -572,39 +568,43 @@ int remote(int recv)
     switch (curs)
     {
     case 2:
-      y = recv * 1000;
+      y_ = recv * 1000;
       break;
     case 3:
-      y = recv * 100 + y;
+      y_ = recv * 100 + y_;
       break;
     case 4:
-      y = recv * 10 + y;
+      y_ = recv * 10 + y_;
       break;
     case 5:
-      y = recv + y;
+      y_ = recv + y_;
       break;
     case 7:
-      mon = recv * 10;
+      mon_ = recv * 10;
       break;
     case 8:
-      mon = recv + mon;
+      mon_ = recv + mon_;
       break;
     case 10:
-      day = 10 * recv;
+      day_ = 10 * recv;
       break;
     case 11:
-      day = day + recv;
+      day_ = day_ + recv;
       break;
     }
     if (curs == 11)
     {
-      lcd.setCursor(0, 0);
-      lcd.print("Input week:(0-7)");
+      curs++;
     }
 
     if (curs == 12)
     {
-      xq = recv;
+      y=y_;
+      day=day_;
+      mon=mon_;
+      #if defined(RTC_DS3231)
+        Rtc.SetDateTime(RtcDateTime{y_,mon_,day_,h,m,s});
+      #endif
       lcd.clear();
       editdate = 0;
       lcd.noBlink();
@@ -625,6 +625,15 @@ int remote(int recv)
 
 void gettime()
 {
+  #if defined(RTC_DS3231)
+    RtcDateTime now = Rtc.GetDateTime();
+    y=now.Year();
+    mon=now.Month();
+    day=now.Day();
+    h=now.Hour();
+    m=now.Minute();
+    s=now.Second();
+  #else
   s++;
   if (s > 59) 
   {
@@ -640,12 +649,8 @@ void gettime()
   {
     h = 0;
     day++;
-    xq++;
   }
-  if (xq > 7)
-  {
-    xq = 1;
-  }
+  #endif
   if (s < 10)
   {
     t[7] = '0' + s;
@@ -692,6 +697,8 @@ void gettime()
   }
   t[2] = ':';
   t[5] = ':';
+  #if defined(RTC_DS3231)
+  #else
   short daymax[13] = {100, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
   if (y / 100 != 0 && y % 4 == 0 || y / 400 == 0)
   {
@@ -711,6 +718,7 @@ void gettime()
     mon = 1;
     y++;
   }
+  #endif
   if (day < 10)
   {
     d[5] = '0' + day;
@@ -734,4 +742,102 @@ void gettime()
   }
   d[0] = '-';
   d[3] = '-';
+  xq=ReturnWeekDay(y,mon,day);
+}
+
+
+uint8_t StringToInt(const char* pString)
+{
+    uint8_t value = 0;
+
+    // skip leading 0 and spaces
+    while ('0' == *pString || *pString == ' ')
+    {
+        pString++;
+    }
+
+    // calculate number until we hit non-numeral char
+    while ('0' <= *pString && *pString <= '9')
+    {
+        value *= 10;
+        value += *pString - '0';
+        pString++;
+    }
+    return value;
+}
+
+
+void inittime()  // 用于初始化时间为编译时间
+{   char *date=__DATE__,*time=__TIME__;
+    // sample input: date = "Dec 06 2009", time = "12:34:56"
+    y = StringToInt(date+9)+2000;
+    // Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
+    switch (date[0])
+    {
+    case 'J':
+        if ( date[1] == 'a' )
+            mon = 1;
+        else if ( date[2] == 'n' )
+            mon = 6;
+        else
+            mon = 7;
+        break;
+    case 'F':
+        mon = 2;
+        break;
+    case 'A':
+        mon = date[1] == 'p' ? 4 : 8;
+        break;
+    case 'M':
+        mon = date[2] == 'r' ? 3 : 5;
+        break;
+    case 'S':
+        mon = 9;
+        break;
+    case 'O':
+        mon = 10;
+        break;
+    case 'N':
+        mon = 11;
+        break;
+    case 'D':
+        mon = 12;
+        break;
+    }
+    day = StringToInt(date + 4);
+    h = StringToInt(time);
+    m = StringToInt(time + 3);
+    s = StringToInt(time + 6);
+    xq=ReturnWeekDay(y,mon,day);
+    for(int i=0;i<TIMEOFFSET;i++)gettime();
+}
+
+
+int ReturnWeekDay( unsigned int iYear, unsigned int iMonth, unsigned int iDay )
+{
+  int iWeek = 0;
+  unsigned int y = 0, c = 0, m = 0, d = 0;
+ 
+  if ( iMonth == 1 || iMonth == 2 )
+  {
+    c = ( iYear - 1 ) / 100;
+    y = ( iYear - 1 ) % 100;
+    m = iMonth + 12;
+    d = iDay;
+  }
+  else
+  {
+    c = iYear / 100;
+    y = iYear % 100;
+    m = iMonth;
+    d = iDay;
+  }
+  
+  iWeek = y + y / 4 + c / 4 - 2 * c + 26 * ( m + 1 ) / 10 + d - 1;    //蔡勒公式
+  iWeek = iWeek >= 0 ? ( iWeek % 7 ) : ( iWeek % 7 + 7 );    //iWeek为负时取模
+  if ( iWeek == 0 )    //星期日不作为一周的第一天
+  {
+    iWeek = 7;
+  }
+  return iWeek;
 }
